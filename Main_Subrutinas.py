6de60deebd999ca06ba2844f026cf8df6290c32c
alexZeLoCO <alejandro.rod.lop@gmail.com>
2020-10-22 09:35:11 +0200

    Nosecuál Commit

---
+++ Main_Subrutinas.py
@@ -0,0 +1,55 @@
+suma = 0        #SUMATORIO DE VALORES           (NUMERADOR)
+contador = 0        #CONTADOR DE VUELTAS        (DENOMINADOR)
+
+#NOTA:
+#   AL UTILIZAR SUBRUTINAS, NOS PODEMOS PERMITIR USAR SÓLO DOS VARIABLES.
+#   UNA PARA LA SUMA Y OTRA PARA EL CONTADOR. PORQUE LAS VARIABLES DENTRO DE UNA SUBRUTINA
+#   EXISTEN SÓLAMENTE EN ESA SUBRUTINA.
+
+#   ESTO ES, CON SÓLO DOS VARIABLES Y TRES SUBRUTINAS, PODEMOS ALMACENAR 6 VALORES:
+#       SUBRUTINA 1: SUMA Y CONTADOR
+#       SUBRUTINA 2: SUMA Y CONTADOR
+#       SUBRUTINA 3: SUMA Y CONTADOR
+
+#   COMO CADA VARIABLE ES DIFERENTE EN CADA SUBRUTINA,
+#   DEBEREMOS DAR EL RESULTADO DESDE DENTRO DE LA SUBRUTINA.
+
+#SUBRUTINA 1 = SOLUCIÓN EJERCICIO 3
+def MediaTotal (suma, n , contador):        #PARÁMETROS SUMA, N Y CONTADOR
+    if n<0:     #SI N ES NEGATIVO (FIN DE BUCLE)
+        return suma/contador        #IMPRIMIR MEDIA
+    else:       #SI NO (EVALUANDO SUMA)
+        suma = suma + n     #ACTUALIZAR SUMATORIO
+        contador = contador + 1     #ACTUALIZAR CONTADOR DE VUELTAS
+
+
+#SUBRUTINAS 2 Y 3 = SOLUCION EJERCICIO 4
+def MediaPares (suma, n , contador):        #PARÁMETROS SUMA, N Y CONTADOR
+    if n<0:        #SI N ES NEGATIVO (FIN DE BUCLE)
+        return suma/contador        #IMPRIMIR MEDIA DE PARES
+    else:       #SI NO (EVALUANDO SUMA)
+        suma = suma + n     #ACTUALIZAR SUMATORIO PARES
+        contador = contador + 1     #ACTUALIZAR CONTADOR DE VUELTAS PARES
+
+def MediaImpares (suma, n, contador):       #PARÁMETROS SUMA, N Y CONTADOR
+    if n<0:     #SI N ES NEGATIVO (FIN DE BUCLE)
+        return suma/contador        #IMPRIMIR MEDIA DE IMPARES
+    else:       #SI NO (EVALUANDO SUMA)
+        suma = suma + n     #ACTUALIZAR SUMATORIO DE IMPARES
+        contador = contador + 1     #ACTUALIZAR CONTADOR DE VUELTAS IMPARES
+
+
+n = int(input ("Introduzca el primer valor: "))     #SOLICITA PRIMER VALOR
+if n < 0:       #SI EL PRIMER VALOR ES NEGATIVO
+    print ("El primer valor ya es negativo, no existe media positiva.")     #OUTPUT
+else:       #SI NO ==> PROGRAMA PRINCIPAL
+    while n>=0:     #MIENTRAS N SEA POSITIVO
+        MediaTotal(suma, n, contador)          #EN CADA VUELTA SE EVALÚA EL VALOR N     (LLAMAMOS SUBRUTINA 1)
+        if n%2==0:      #EN CADA VUELTA EN LA QUE N SEA PAR
+            MediaPares(suma,n,contador)     #SE EVALÚA EL VALOR PAR                     (LLAMAMOS SUBRUTINA 2)
+        else:           #SI NO ES PAR, ES IMPAR
+            MediaImpares(suma,n,contador)   #SE EVALÚA EL VALOR IMPAR                   (LLAMAMOS SUBRUTINA 3)
+
+    print("La media de todos es:",MediaTotal(suma,n,contador))              #OUTPUT
+    print("La media de los pares es:",MediaPares(suma,n,contador))          #OUTPUT
+    print("La media de los impares es:",MediaImpares(suma,n,contador))      #OUTPUT
--- main.py
+++ main.py
@@ -1,15 +1,46 @@
+#INICIALIZA SUMA            (NUMERADOR) EJERCICIO 3
+suma = 0
+#INICIALIZA SUMA PARES      (NUMERADOR) EJERCICIO 4
+sumapares = 0
+#INICIALIZA SUMA IMPARES    (NUMERADOR) EJERCICIO 4
+sumaimpares = 0
+#INICIALIZA CONTADOR        (DENOMINADOR)   EJERCICIO 3
+contador = 0
+#INICIALIZA CONTADOR PARES  (DENOMINADOR)     EJERCICIO 4
+contadorpares = 0
+#INICIALIZA CONTADOR IMPARES (DENOMINADOR)  EJERCICIO 4
+contadorimpares = 0
+
 n=int (input("Introduzca el primer valor: "))       #SOLICITA PRIMER VALOR
-suma = 0        #INICIALIZA SUMA        (NUMERADOR)
-contador = 0    #INICIALIZA CONTADOR    (DENOMINADOR)
 
 while n>=0:     #MIENTRAS N SEA MAYOR QUE 0
-    suma = suma + n     #ACTUALIZAR SUMA
-    contador = contador + 1     #ACTUALIZAR CONTADOR
-    n = float (input("Introduzca el siguiente valor: "))        #SOLICITA SIGUIENTE VALOR
 
-if suma == 0:       #SI EL PRIMER VALOR ES NEGATIVO, EL DENOMINADOR SERÍA 0
-                    #(NO SE ENTRÓ EN EL WHILE ==> CONTADOR = 0)
+    contador = contador + 1  # ACTUALIZAR CONTADOR
+    suma = suma + n  # ACTUALIZAR SUMA
 
-    contador = 1    #VALOR DE CONTADOR = 1
+    if n%2==0:      #SI ES PAR
+        contadorpares = contadorpares + 1  # ACTUALIZAR CONTADOR
+        sumapares=sumapares+n       #ACTUALIZAR SUMA
 
-print ("La media de los números introducidos es:",suma/contador)        #OUTPUT
+    if n%2!=0:      #SI ES IMPAR
+        contadorimpares = contadorimpares + 1  # ACTUALIZAR CONTADOR
+        sumaimpares=sumaimpares+n   #ACTUALIZAR SUMA
+
+    n = float(input("Introduzca el siguiente valor: "))  # SOLICITA SIGUIENTE VALOR
+
+
+#DEBEMOS REVISAR SI SE HAN INTRODUCIDO VALORE DE TODOS LOS TIPOS.
+#SI NO, DIVIDIRÍAMOS ENTRE 0. DAREMOS VALOR 1 EN SU LUGAR.
+if suma == 0:       #SI SUMA TOTAL ES 0
+    contador = 1    #CONTADOR 1
+    suma = 1        #SUMA 1
+if sumapares == 0:      #SI SUMA PARES ES 0
+    contadorpares = 1   #CONTADOR PARES 1
+    sumapares = 1       #SUMA PARES 1
+if sumaimpares == 0:        #SI SUMA IMPARES ES 0
+    contadorimpares = 1     #CONTADOR IMPARES 1
+    sumaimpares = 1         #CONTADOR IMPARES 1
+
+print("La media de los números introducidos es:", suma/contador)  # OUTPUT
+print("La media de los números pares introducidos es:", sumapares/contadorpares)  # OUTPUT
+print("La media de los números impares introducidos es:", sumaimpares/contadorimpares)  # OUTPUT
